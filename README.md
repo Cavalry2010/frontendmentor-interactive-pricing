64307df7d6c285d9b4d3d44cc1b8761c0d620bed
Cavalry2010 <el.cavalry.2010@gmail.com>
2023-05-10 02:43:13 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Frontend Mentor - Interactive pricing component solution
+
+This is a solution to the [Interactive pricing component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-pricing-component-t0m8PIyY8). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Use the slider and toggle to see prices for different page view numbers
+
+### Screenshot
+
+![Desktop Preview](./images/screenshot.png)
+
+### Links
+
+- Solution URL: [Frontend Mentor Solution](https://your-solution-url.com)
+- Live Site URL: [Interactive Pricing Component](https://interactive-pricing-alejandro.netlify.app)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Desktop-first workflow
+- JavaScript (ES6)
+
+### What I learned
+
+This challenge helped me learn more on updating the DOM dinamically and about range inputs!
+
+## Author
+
+- Frontend Mentor - [@Cavalry2010](https://www.frontendmentor.io/profile/Cavalry2010)
+- Twitter - [@AlPerdomoMC](https://www.twitter.com/AlPerdomoMC)
+- GitHub - [@Cavalry2010](https://www.github.com/Cavalry2010)
---
+++ js/script.js
@@ -0,0 +1,84 @@
+"use strict";
+
+class DraggableSelector {
+  form = document.querySelector(".pricing-form");
+  input = document.querySelector("#price-selector");
+  pricingDetails = document.querySelector(".pricing-details");
+  pageviews = document.querySelector(".pageviews-num");
+  price = document.querySelector(".price");
+  timeframe = document.querySelector(".timeframe");
+  billPlanBtn = document.querySelector(".billing-plan-btn");
+  plans = [
+    ["10K", 8],
+    ["50K", 12],
+    ["100K", 16],
+    ["500K", 24],
+    ["1M", 36],
+  ];
+  range;
+  discount;
+
+  constructor() {
+    this.init();
+    this.billPlanBtn.addEventListener("click", this.switchPlan.bind(this));
+    this.form.addEventListener("input", this.checkValue.bind(this));
+    this.form.addEventListener(
+      "submit",
+      function (e) {
+        this.showResults(e);
+      }.bind(this)
+    );
+  }
+
+  checkValue() {
+    const range = +this.input.value;
+    const value = (range / this.input.max) * 100;
+    this.range = range;
+    this.input.style.background = `linear-gradient(to right, #a5f3eb 0%, #a5f3eb ${value}%, #eaeefb ${value}%, #eaeefb 100%)`;
+    this.updatePrice(range);
+  }
+
+  updatePrice(range) {
+    this.pricingDetails.classList.toggle("transition");
+    const timeframe = this.discount === 1 ? "month" : "year";
+    setTimeout(
+      function () {
+        this.pageviews.textContent = this.plans[range][0];
+        this.price.textContent = `$${(
+          this.plans[range][1] * this.discount
+        ).toFixed(2)}`;
+        this.timeframe.textContent = timeframe;
+        this.pricingDetails.classList.toggle("transition");
+      }.bind(this),
+      200
+    );
+  }
+
+  switchPlan() {
+    if (this.discount === 1) {
+      this.discount = 0.75;
+    } else {
+      this.discount = 1;
+    }
+    this.billPlanBtn.classList.toggle("billing-plan-btn--active");
+    this.updatePrice(this.range);
+  }
+
+  showResults(e) {
+    e.preventDefault();
+    console.log(
+      `Selected Plan: ${this.plans[this.range][0]} pageviews for $${(
+        this.plans[this.range][1] * this.discount
+      ).toFixed(2)} / ${this.discount === 1 ? "month" : "year"}`
+    );
+    console.log("App Finished!");
+  }
+
+  init() {
+    this.range = 2;
+    this.discount = 1;
+    this.updatePrice(this.range);
+  }
+}
+
+const app = new DraggableSelector();
